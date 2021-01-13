41b594d67f52afdb75758978571e082ea4aaccea
Maxwell Volz <maxwellvolz@gmail.com>
2021-01-12 17:54:43 -0800

    day 8 part 2 complete

--- adventofcode_day_8.py
+++ adventofcode_day_8.py
@@ -53,29 +53,28 @@ def execute_line(accumulator, line_number):
     else:
         execute_line(accumulator, line_number)
 
+
 # execute_line(0, 0)
 
 
+def execute_line2(modded_boot_code, accumulator, line_number, lines_executed_check=[]):
 
+    instruction = modded_boot_code[line_number]
+    # print(
+    #     "Executing line:",
+    #     line_number,
+    #     "| accumulator:",
+    #     accumulator,
+    #     "| instruction:",
+    #     instruction,
+    # )
 
-def execute_line2(accumulator, line_number):
+    if line_number in lines_executed_check:
+        # print("Duplicate line attempted:", line_number, lines_executed_check)
+        return
 
-    instruction = boot_code[line_number]
-    print("Executing line:", line_number, "| accumulator:", accumulator, "| instruction:", instruction)
-
-    if line_number in lines_executed:
-        print('Found the bug! Line:', line_number,'repeated...', lines_executed)
-        if "jmp" in boot_code[lines_executed[-1]]:
-            instruction = boot_code[lines_executed[-1]].replace('jmp', 'nop')
-            print(instruction)
-        else:
-            instruction = boot_code[lines_executed[-1]].replace('nop', 'jmp')
-        line_number = lines_executed[-1]
-        lines_executed.pop()
-        print(lines_executed)
-        print("Replaced instruction:",instruction)
     else:
-        lines_executed.append(line_number)
+        lines_executed_check.append(line_number)
 
     if "acc" in instruction:
         increment_value = int(instruction.split(" ")[1][1:])
@@ -84,21 +83,43 @@ def execute_line2(accumulator, line_number):
         else:
             accumulator -= increment_value
         line_number += 1
-
     elif "jmp" in instruction:
         jump_value = int(instruction.split(" ")[1][1:])
         if "+" in instruction:
             line_number += jump_value
         else:
             line_number -= jump_value
-
     else:
         line_number += 1
 
-    if len(lines_executed) == total_possible_lines:
+    if line_number == len(modded_boot_code):
         print("\nAccumulator:", accumulator)
         return
-    else:
-        execute_line2(accumulator, line_number)
 
-execute_line2(0, 0)
+
+    execute_line2(modded_boot_code, accumulator, line_number, lines_executed_check)
+
+f = open("data/day_8_part_2.txt")
+test_data_part_2 = "".join(f.readlines())
+
+# print(test_data_part_1)
+
+boot_code = test_data_part_2.split("\n")
+
+
+to_check = [i for i, x in enumerate(boot_code) if "jmp" in x or "nop" in x]
+
+print(to_check)
+for swap_index in to_check:
+    # print("swapping:", swap_index)
+    modded_boot_code = boot_code[:]
+
+    line_to_mod = modded_boot_code[swap_index]
+    if "jmp" in line_to_mod:
+        modded_boot_code[swap_index] = line_to_mod.replace("jmp", "nop")
+    else:
+        modded_boot_code[swap_index] = line_to_mod.replace("nop", "jmp")
+
+    # print(modded_boot_code)
+    # print(modded_boot_code[swap_index])
+    execute_line2(modded_boot_code, 0, 0, [])
